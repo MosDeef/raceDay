9cae15fc4c2bc9ff97e1048d4237f3caa64f6daa
MosDeef <114076486+MosDeef@users.noreply.github.com>
2022-10-31 16:57:44 +1100

    Create main.js

---
+++ main.js
@@ -0,0 +1,20 @@
+// I like to think I could have come up with this code if you hadn't provided it, but we'll see
+let raceNumber = Math.floor(Math.random() * 1000);
+
+// a litte boolean to see when they came onboard
+let earlyReg = true
+let runnerAge = 18
+console.log(raceNumber);
+
+// early birds get higher numbers
+if (earlyReg == true && runnerAge >= 18) {
+    (raceNumber += 1000)
+}
+// slackers and children, less so
+if (earlyReg == true && runnerAge >= 18) {
+        console.log(`You'll race at 9:30am ${raceNumber}`)
+} else if (earlyReg == false && runnerAge >= 18) {
+    console.log(`You'll race at 11am ${raceNumber}`)
+} else if (runnerAge < 18) {
+    console.log(`You'll race at 12:30pm ${raceNumber}`)
+}
